01431b8ddcf9156bae3191d8c610705ecb9320fd
k22rs131 <k22rs131@st.kyusan-u.ac.jp>
2025-04-15 15:08:06 +0900

    構文エラーの修正

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
-import environ
+#import environ
 from decouple import config
 from dj_database_url import parse as dburl
 
