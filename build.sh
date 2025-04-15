d8d29775deb363f9ce2ea703b41268c551069daa
k22rs131 <k22rs131@st.kyusan-u.ac.jp>
2025-04-15 16:07:19 +0900

    消去

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ set -o errexit
 
 pip install -r requirements.txt
 
-git config --global user.email "k22rs131@st.kyusan-u.ac.jp"
-git config --global user.name "k22rs131"
+#git config --global user.email "k22rs131@st.kyusan-u.ac.jp"
+#git config --global user.name "k22rs131"
 
 
-git add requirements.txt
-git commit -m "fix: add python-decouple"
-git push
+#git add requirements.txt
+#git commit -m "fix: add python-decouple"
+#git push
 
 python manage.py collectstatic --no-input
 python manage.py migrate
