addad2fce92ff861dbdd11eacc93dc0012035403
k22rs131 <k22rs131@st.kyusan-u.ac.jp>
2025-04-15 16:17:29 +0900

    environのインストール

--- build.sh
+++ build.sh
@@ -5,11 +5,14 @@
 set -o errexit
 
 pip install -r requirements.txt
+pip install django-environ
 
 #git config --global user.email "k22rs131@st.kyusan-u.ac.jp"
 #git config --global user.name "k22rs131"
 
-
+git add requirements.txt
+git commit -m "fix: add django-environ to requirements"
+git push
 #git add requirements.txt
 #git commit -m "fix: add python-decouple"
 #git push
