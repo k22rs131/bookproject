b1c3eb6009edf05bc118a5d7180564da0756b9ca
k22rs131 <k22rs131@st.kyusan-u.ac.jp>
2025-04-15 15:16:00 +0900

    python-decoupleのインストール

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
+git add requirements.txt
+git commit -m "fix: add python-decouple"
+git push
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py superuser
