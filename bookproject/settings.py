993bc6b55c07157a10dd416baa413a1b0442c4a3
k22rs131 <k22rs131@st.kyusan-u.ac.jp>
2025-04-21 15:22:05 +0900

    一時的にデバックをTrueに

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -32,9 +32,10 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = 'django-insecure-feb#f5t-at9v1r)1i3al9h5(6*9rz-=lsihzo#i=^+ekfir(ey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com']
+#ALLOWED_HOSTS = ['*']
 
 
 # Application definition
