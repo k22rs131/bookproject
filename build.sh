a19f1251499474891b467e706d497634441f08eb
k22rs131 <k22rs131@st.kyusan-u.ac.jp>
2025-04-28 14:59:02 +0900

    シークレットキーの設定

--- build.sh
+++ build.sh
@@ -4,11 +4,8 @@
 # exit on error
 set -o errexit
 
-git config --global user.email "k22rs131@st.kyusan-u.ac.jp"
-git config --global user.name "k22rs131"
-
 pip install -r requirements.txt
-pip install django-environ
+
 
 python manage.py collectstatic --no-input
 python manage.py migrate
